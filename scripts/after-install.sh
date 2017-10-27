657576dfeadd1eace9bb000ee37bab1f621b57bf
Omar Ramadan <oramadan@fb.com>
2017-10-27 12:21:24 -0700

    Add gtp.ko back into depmod

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -3,7 +3,9 @@ DEPMOD_CONFIG_FILE=${DEPMOD_CONFIG_DIR}/gtp.conf
 
 mkdir -p ${DEPMOD_CONFIG_DIR}
 echo "override gtp * extra" >> ${DEPMOD_CONFIG_FILE}
-echo "override gtp * weak-updates" >> ${DEPMOD_CONFIG_FILE}
+echo "override gtp * extra" >> ${DEPMOD_CONFIG_FILE}
+echo "override gtp.ko * weak-updates" >> ${DEPMOD_CONFIG_FILE}
+echo "override gtp.ko * weak-updates" >> ${DEPMOD_CONFIG_FILE}
 
 echo "Running depmod..."
 depmod `uname -r`
